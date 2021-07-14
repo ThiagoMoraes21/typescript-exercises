ffa5b57c66d0cdb76f8cdb5cdddc08989ab08155
thiagoMoraes21 <thiago4x70@gmail.com>
2021-07-14 12:44:32 -0300

    solved first exercise

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ The goal: Let everyone play with many different TypeScript features and get an o
 
 ## Things to cover
 
-        1. Basic typing.
-        2. Refining types.
-        3. Union types.
-        4. Merged types.
-        5. Generics.
-        6. Type declarations.
-        7. Module augmentation.
-        8. Advanced type mapping.
+1. Basic typing.
+2. Refining types.
+3. Union types.
+4. Merged types.
+5. Generics.
+6. Type declarations.
+7. Module augmentation.
+8. Advanced type mapping.
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
---
+++ exercise-1/index.js
@@ -0,0 +1,40 @@
+"use strict";
+/*
+    Intro:
+
+    We are starting a small community of users. For performance
+    reasons we have decided to store all users right in the code.
+    This way we can provide our developers with more
+    user-interaction opportunities. With user-related data, at least.
+    All the GDPR-related issues we will solved some other day.
+    This would be the base for our future experiments during
+    these exercises.
+
+    Exercise:
+
+    Given the data, define the interface "User" and use it accordingly.
+
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.users = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+function logPerson(user) {
+    console.log(" - " + user.name + ", " + user.age);
+}
+exports.logPerson = logPerson;
+console.log('Users:');
+exports.users.forEach(logPerson);
+/* In case if you are stuck:
+
+// https://www.typescriptlang.org/docs/handbook/interfaces.html#introduction
+*/
